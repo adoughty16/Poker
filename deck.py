f712d99616380f9842fab295ac4e11398af74297
cjrinaldi <cjrinald@uvm.edu>
2023-10-11 17:11:07 -0400

    deck class pseudocode

---
+++ deck.py
@@ -0,0 +1,7 @@
+# DECK:
+# -Cards[]
+# get_deck()
+# deal() returns 4-tuple of lists of 2 cards each and removes them from Cards[]
+# flop() returns list of 3 cards and removes them from Cards[]
+# turn() returns 1 card and removes it from Cards[]
+# river() returns 1 card and removes it from Cards[]
