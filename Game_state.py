72271727a46707666190ff94f55ead3540884004
Sydney White <sydney.white@uvm.edu>
2023-10-19 14:19:15 -0400

    variables in game_state

--- Game_state.py
+++ Game_state.py
@@ -51,10 +51,53 @@ class Game_state:
 
     def __init__(self):
         #populate variables
+        self.players = []
+        # store card objects (?)
+        self.community_cards = []
+        # to bet updated each time
+        self.pot = 0
+        # hard code first blind to be 10
+        self.bet = 10
+        # index of the dealer
+        self.dealer = 0
+        # players left: array of int indexes (important for showdown round)
+        self.actives = [0, 1, 2, 3]
 
         #start the game dealing
         self.game_state = 'dealing'
-        return
+
+    def set_players(self, players):
+        for player in players:
+            self.players.append(player.name)
+
+    def set_community_cards(self, community_cards):
+        # add to the community cards
+        # self.community_cards.append()
+        # based on where these are decided, maybe add makes more sense and call twice in flop
+    
+
+    def set_pot(self, pot):
+        self.pot = pot
+
+    def set_bet(self, new_bet):
+        # wherever this is called, only call if new_bet > bet
+        self.bet = new_bet
+
+    def set_dealer(self):
+        # dealer is the index of the dealer
+        # use this index and add (and mod) to get other players such as blind and double_blind
+        self.dealer += 1
+        self.dealer % 4
+
+    # set_actives is really just removing anyone who folds
+    def remove_player(self, player):
+        self.actives.remove(player.name)
+
+    def set_round(self, round):
+        # updates the enumerated type for round after the right number of turns have passed (depending on len(actives))
+        # do we need to check that this is "next in line"?
+        self.round = round
+
 
     def upload(self):
         return
