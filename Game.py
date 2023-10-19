f9d901901984b35e3d795bdfcb5e48c9f758c5be
adoughty16 <adoughty16@gmail.com>
2023-10-19 11:44:45 -0400

    added host confirmation loop

--- Game.py
+++ Game.py
@@ -16,21 +16,37 @@ class Game:
 		self.host = host
 		self.db = db
 
-
-	def initial_main(): #determines which game loop to call
-		if (host):
-			host_main()
+	def initial_main(self): #determines which game loop to call
+		if (self.host):
+			self.host_main()
 		else:
-			guest_main()
+			self.guest_main()
 		pass
 
 	def host_main(self):
-		pass
 		# Needs to establish and confirm connection with guests VIA firestore before game loop
+		playing = True
+		connected = False
+		#first spots in flags hold 0s for players to flip to 1s when they connect.
+		#last spot is host confirmation bit that host will flip when all player spots are filled
 		flags = [0 for _ in range(self.players + 1)]
 		self.db.collection("flags").document("flag_document").set({"values": flags})
-		
+		flag_document = self.db.collection("flags").document("flag_document").get()
 
+		while not connected:
+			#update local flags from database
+			flags = flag_document.to_dict()["values"]
+			connected = True
+			for i in range(self.players - 1):
+				#if any are 0
+				if flags[i] == 0:
+					#not everyone is connected yet
+					connected = False
+			#if all players have connected
+			if connected:
+				#update and upload confirmation bit
+				flags[self.players] = 1
+				self.db.collection("flags").document("flag_document").set({"values": flags})
 
 
 		#uploads to firestore indicating how many live players are expected (these can just be bits set to zero)
@@ -45,20 +61,22 @@ class Game:
 		#Needs to establish and confirm connection to host VIA firestore before game loop
 
 		while not connected:
-			#check firestore for host's flag
+			#check flags on database
 			flag_document = self.db.collection("flags").document("flag_document").get()
 			if flag_document.exists:
 				#grab flags from db
 				flags = flag_document.to_dict()["values"]
 				#if we haven't reserved a spot in the game
 				if not waiting_for_host:
-					#look through flags until we find an opening
+					#look through all flags except the confirmation bit at the end until we find an opening
 					for i in range(self.players - 1):
 						if flags[i] == 0:
 							#reserve the opening
 							flags[i] = 1
 						#break out so we only reserve one spot
 						break
+					#update flags on the database
+					self.db.collection("flags").document("flag_document").set({"values": flags})
 					#now we just need to check for the host's confirmation bit
 					waiting_for_host = True
 				#if we are awaiting confirmation
@@ -71,6 +89,7 @@ class Game:
 
 		#while playing is true:
 		while (playing):
+			pass
 
 			#fetch_game_state() until it is your turn
 
@@ -93,11 +112,7 @@ class Game:
 
 
 				#your turn is false
-
-
-
-
-
+	
 	def find_best_hand():
 		#calls each player's evaluate_hand() and determines the winner. If there is a tie, it can 
 		#systematically search through individual hands for the highest card.
@@ -109,7 +124,7 @@ class Game:
 	def fetch_game_state(): #fetches game-state from server
 		pass
 
-	def upload_turn() #guess uploads turn
+	def upload_turn(): #guess uploads turn
 		pass
 
 	def fetch_turn(player_index): # host fetches guest turn and updates game state
