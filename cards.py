cc31704b6c21b1005b2ed9e02d979d18bcfaa843
cjrinaldi <cjrinald@uvm.edu>
2023-10-19 15:03:14 -0400

    added compareTo function

--- cards.py
+++ cards.py
@@ -21,7 +21,19 @@ class Card:
         self.suit=suit
         self.value=value
 
-
+    #compare to function
+    #if the card passed in is greater than this card, return 1
+    #if the card passed in is less than this card, return -1
+    #if the card passed in is equal to this card, return 0
+    def compareTo(this_card):
+        if (this_card.value>this.card.value):
+            return 1
+        if (this_card.value<this.card.value):
+            return -1
+        if (this_card.value==this.card.value):
+            return 0
+        
+    
     def get_suit(self):
         return self.suit
     def get_value(self):
