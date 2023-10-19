af036f8cff7378448fbef919adbdcbef1ccac295
Xantia Que Kansil <xantia.kansil@uvm.edu>
2023-10-19 15:12:20 -0400

    Upload New File

---
+++ Player.py
@@ -0,0 +1,114 @@
+import cards
+from enum import Enum
+from collections import Counter
+
+
+class HandStrength(Enum):
+    HIGH_CARD = 1
+    ONE_PAIR = 2
+    TWO_PAIR = 3
+    THREE_OF_A_KIND = 4
+    STRAIGHT = 5
+    FLUSH = 6
+    FULL_HOUSE = 7
+    FOUR_OF_A_KIND = 8
+    STRAIGHT_FLUSH = 9
+    ROYAL_FLUSH = 10
+
+class Player:
+    def __init__(self, name, is_computer_player=False):
+        self.name = name
+        self.hand = []
+        self.stack = 0
+        self.is_computer_player = is_computer_player
+
+    def turn(self, community_cards):
+        # return decision (bet, check, fold)
+        decision = self.make_decision(community_cards)
+        return decision
+
+    def evaluate_strength(self, community_cards):
+        # takes community_cards, combines with self.hand, returns strength
+        combined_cards = self.hand + community_cards
+        hand_strength = self.calculate_strength(combined_cards)
+        return hand_strength
+
+
+    def evaluate_hand(self, community_cards):
+        #  takes in community_cards, combines them with self.hand, and returns the ENUM for the player's hand.
+        combined_cards = self.hand + community_cards
+        hand_type = self.get_hand_type(combined_cards)
+        return hand_type
+
+    def make_decision(self, community_cards):
+        # could use evaluate_strength and evaluate_hand as part of decision
+        # random decision for now
+        import random
+        decisions = ["bet", "check", "fold"]
+        return random.choice(decisions)
+
+    def calculate_strength(self, cards):
+        # Implement hand strength evaluation
+        # Sort the cards by rank (e.g., 2, 3, 4, ..., A)
+        sorted_cards = sorted(cards, key=lambda card: Card.RANK_ORDER.index(card.rank))
+        counts = Counter(card.rank for card in sorted_cards)
+
+        if self.is_royal_flush(sorted_cards):
+            return HandStrength.ROYAL_FLUSH
+        elif self.is_straight_flush(sorted_cards):
+            return HandStrength.STRAIGHT_FLUSH
+        elif self.is_four_of_a_kind(counts):
+            return HandStrength.FOUR_OF_A_KIND
+        elif self.is_full_house(counts):
+            return HandStrength.FULL_HOUSE
+        elif self.is_flush(sorted_cards):
+            return HandStrength.FLUSH
+        elif self.is_straight(sorted_cards):
+            return HandStrength.STRAIGHT
+        elif self.is_three_of_a_kind(counts):
+            return HandStrength.THREE_OF_A_KIND
+        elif self.is_two_pair(counts):
+            return HandStrength.TWO_PAIR
+        elif self.is_one_pair(counts):
+            return HandStrength.ONE_PAIR
+        else:
+            return HandStrength.HIGH_CARD
+    def get_hand_type(self, cards):
+        # Implement hand type evaluation logic
+        # Return the appropriate ENUM from HandStrength
+
+        def is_royal_flush(self, cards):
+            # Check if it's a Royal Flush (A, K, Q, J, 10 of the same suit)
+            pass
+
+        def is_straight_flush(self, cards):
+            # Check if it's a Straight Flush
+            pass
+
+        def is_four_of_a_kind(self, counts):
+            # Check if it's Four of a Kind
+            pass
+
+        def is_full_house(self, counts):
+            # Check if it's a Full House
+            pass
+
+        def is_flush(self, cards):
+            # Check if it's a Flush
+            pass
+
+        def is_straight(self, cards):
+            # Check if it's a Straight
+            pass
+
+        def is_three_of_a_kind(self, counts):
+            # Check if it's Three of a Kind
+            pass
+
+        def is_two_pair(self, counts):
+            # Check if it's Two Pair
+            pass
+
+        def is_one_pair(self, counts):
+            # Check if it's One Pair
+            return 2 in counts.values()
