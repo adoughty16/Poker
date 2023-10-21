cc29473031ec4bbf6e7898e63b476d605c30714d
swhite25 <sydney.white@uvm.edu>
2023-10-21 12:42:42 -0400

    added database object and updates

--- Game_state.py
+++ Game_state.py
@@ -1,5 +1,5 @@
 from enum import Enum
-
+from db_connect import db 
 
 """
 pre-flop:After the Dealer has passed out two cards to everyone, 
@@ -30,6 +30,8 @@ class Game_state:
     def __init__(self):
         #populate variables
         self.player_names = []
+        # added player hands to be set and "got" only in showdown (for built-in security)
+        self.player_hands = []
         # store card objects (?)
         self.community_cards = []
         # to bet updated each time
@@ -43,16 +45,23 @@ class Game_state:
         self.dealer = 0
         # players left: array of int indexes (important for showdown round)
         self.actives = [0, 1, 2, 3]
-
         #first round is dealing
         self.round = 'dealing'
-
         self.player_decision = 'check'
+
+        # create a document in the database for the game's gamestate
+        # ultimately, we decided to make one document and update it as needed to theoretically support multiple games at once
+        # NOTE: player_hands and comunity_cards means that the card class with need a to_dict and from_dict 
+        data = {"player_names": self.player_names, "player_hands": self.player_hands, "community_cards": self.community_cards, 
+        "total_pot": total_pot, "round_pot": round_pot, "bet": self.bet, "minimum_call": self.minimum_call, "dealer": self.dealer,
+        "actives": self.actives, "round": self.round, "player_decision": self.player_decision}
+        game_state_ref = db.collection("").add(data)
 
 
     def set_players(self, players):
         for player in players:
             self.players.append(player.name)
+        game_state_ref.udpate({"player_names": self.players})
 
     def set_community_cards(self, community_cards):
         # add to the community cards
@@ -62,42 +71,52 @@ class Game_state:
     
     def set_total_pot(self, pot):
         self.total_pot = pot
+        game_state_ref.update({"total_pot": self.total_pot})
     
     def set_round_pot(self, round_pot)
         self.round_pot = round_pot
+        game_state_ref.update({"round_pot": self.round_pot})
 
     def set_bet(self, new_bet):
         # wherever this is called, only call if new_bet > bet
         self.bet = new_bet
+        game_state_ref.update({"bet": self.bet})
     
     def set_minimum_call(self, minimum_call):
         self.minimum_call = minimum_call
+        game_state_ref.update({"minimum_call": self.minimum_call})
 
     def set_player_decision(self, player_decision):
         self.player_decision = player_decision
+        game_state_ref.update({"player_decision": self.player_decision})
 
-    def set_bet(self, new_bet):
-        # wherever this is called, only call if new_bet > bet
-        self.bet = new_bet
+    def set_dealer(self, dealer):
+        # dealer is the index of the dealer
+        self.dealer = dealer
+        game_state_ref.udpate({"dealer": self.dealer})
 
-    def set_dealer(self):
+    def update_dealer(self):
         # dealer is the index of the dealer
         # use this index and add (and mod) to get other players such as blind and double_blind
         self.dealer += 1
         self.dealer % 4
+        game_state_ref.udpate({"dealer": self.dealer})
 
     # set_actives is really just removing anyone who folds
     def remove_player(self, player):
         self.actives.remove(player.name)
+        game_state_ref.update({"actives": self.actives})
 
     def set_round(self, round):
         # updates the enumerated type for round after the right number of turns have passed (depending on len(actives))
         # do we need to check that this is "next in line"?
         self.round = round
+        game_state_ref.udpate({"round": self.round})
 
 # getters 
     def get_players(self):
-        return self.players
+        doc = game_state_ref.get()
+        return doc.players
 
     def get_community_cards(self):
         # add to the community cards
@@ -137,6 +156,8 @@ class Game_state:
        return self.round
 
 # other funcitons 
+    # i'm confused by what the difference between upload and upload_turn are - this is only called once (so far) in the game loop
+    # i didn't think we would need to store player decisions or anything like that and could just update whatever attribute specifically changed in the game loop 
     def upload(self, db): #uploads game_state to db
         pass
     
