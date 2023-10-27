92bc0680d3e13eb7c452fb9aacaa7db22b44467b
adoughty16 <adoughty16@gmail.com>
2023-10-27 11:26:56 -0400

    added game comments

--- Game.py
+++ Game.py
@@ -165,18 +165,28 @@ class Game():
 							self.game_state.set_player_decision(choice, self.db)
 							self.game_state.increment_whose_turn()
 							lock.release()
-							
+						
+						#if AI decides to fold
 						elif choice == 'fold':
+							#remove their index from actives[]
 							self.actives.remove(self.current)
+							#tell gamestate
 							lock.aquire()
 							self.game_state.remove_player(self.current, self.db)
 							lock.release()
 
+						# if AI decides to call
 						elif choice == 'call':
+							# add to the pot the call amount (based on the amount below the total_call the current player
+							# has already bet in round_bets)
 							self.pot += (self.total_call - self.round_bets[self.current])
+							#reflect that in the player's stack
 							self.players[self.current].set_stack(self.player[self.current].get_stack - (self.total_call - self.round_bets[self.current]))
+							#set that players round_bets to the call value
 							self.round_bets[self.current] = self.total_call
+							#update local stacks with the new player stack
 							self.stacks[self.current] = self.players[self.current].get_stack()
+							#reflect the changes in the gamestate
 							lock.aquire()
 							self.set_player_stacks(self.stacks)
 							self.game_state.set_round_pot(self.game_state.get_round_pot(self.db) + value, self.db)
@@ -211,38 +221,63 @@ class Game():
 						# player turn
 
 						choice, value = self.game_state.get_player_decision(self.db)
+						#if bet
 						if choice == 'bet':
-							self.pot += value
+							#compute the amount of money this player is putting into the pot:
+							#	The value is the amount over the minimum call that the player is putting into the pot
+							#	To compute the total amount you just take the total_call minus the amount the player has
+							#	already bet that round. Then you add the bet value.
+							bet_amount = ((self.total_call - self.round_bets[self.current]) + value)
+							#pot goes up by bet amount
+							self.pot += bet_amount
+							#total_call goes up by the bet value
 							self.total_call += value
-							self.round_bets[self.current] += value
-							self.players[self.current].set_stack(self.player[self.current].get_stack - value)
+							#add the bet_amount to the round_bets for the current player
+							self.round_bets[self.current] += bet_amount
+							#subtract that amount from the player's stack
+							self.players[self.current].set_stack(self.player[self.current].get_stack - bet_amount)
+							#update local stacks
 							self.stacks[self.current] = self.players[self.current].get_stack()
+							#now reflect those changes in the gamestate
 							lock.aquire()
 							self.set_player_stacks(self.stacks)
-							self.game_state.set_round_pot(self.game_state.get_round_pot(self.db) + value, self.db)
+							self.game_state.set_round_pot(self.game_state.get_round_pot(self.db) + bet_amount, self.db)
 							self.game_state.set_bet(value, self.db)
 							self.game_state.set_total_call(self.game_state.get_total_call() + value, self.db)
 							self.game_state.set_player_decision(choice, self.db)
 							self.game_state.increment_whose_turn()
 							lock.release()
-
+						
+						#if check
 						elif choice == 'check':
+							#basically just skipping their turn
+							#reflect in game state
 							lock.aquire()
 							self.game_state.set_player_decision(choice, self.db)
 							self.game_state.increment_whose_turn()
 							lock.release()
-							
+						
+						#if fold
 						elif choice == 'fold':
+							#remove their index from actives[]
 							self.actives.remove(self.current)
+							#tell gamestate
 							lock.aquire()
 							self.game_state.remove_player(self.current, self.db)
 							lock.release()
 
+						# if call
 						elif choice == 'call':
+							# add to the pot the call amount (based on the amount below the total_call the current player
+							# has already bet in round_bets)
 							self.pot += (self.total_call - self.round_bets[self.current])
+							#reflect that in the player's stack
 							self.players[self.current].set_stack(self.player[self.current].get_stack - (self.total_call - self.round_bets[self.current]))
+							#set that players round_bets to the call value
 							self.round_bets[self.current] = self.total_call
+							#update local stacks with the new player stack
 							self.stacks[self.current] = self.players[self.current].get_stack()
+							#reflect the changes in the gamestate
 							lock.aquire()
 							self.set_player_stacks(self.stacks)
 							self.game_state.set_round_pot(self.game_state.get_round_pot(self.db) + value, self.db)
@@ -251,9 +286,8 @@ class Game():
 							self.game_state.set_player_decision(choice, self.db)
 							self.game_state.increment_whose_turn()
 							lock.release()
-						lock.release()
 
-					#change current player
+					#increment current player based on actives[]
 					self.current += 1
 					while self.current not in self.actives:
 						self.current += 1
@@ -267,6 +301,8 @@ class Game():
 						self.stacks[self.actives[0]] = self.players[self.actives[0]].get_stack()
 						#reset the game to dealing values
 						self.round_bets = [0, 0, 0, 0]
+						self.actives = [1, 2, 3, 4] #NOTE: THIS IS A PLACEHOLDER AND WILL NOT WORK AFTER SOMEONE 
+													# HAS BUSTED OUT OF THE GAME
 						self.pot = 0
 						lock.aquire()
 						self.set_player_stacks(self.stacks)
