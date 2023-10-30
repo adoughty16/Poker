75b7a54a440e7242dfc94e74ec4a7334312bd5bb
xantia.kansil <xantia.kansil@uvm.edu>
2023-10-30 11:44:47 -0400

    deleted logic from pulling/moving cards (from practicing what py arcade can do)

--- Graphics.py
+++ Graphics.py
@@ -733,36 +733,6 @@ class GameView(arcade.View):
         # round_pot
 
 
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """ Called when the user presses a mouse button. """
-        # Get list of cards we've clicked on
-        cards = arcade.get_sprites_at_point((x, y), self.card_list)
-
-        # Have we clicked on a card?
-        if len(cards) > 0:
-            # Might be a stack of cards, get the top one
-            primary_card = cards[-1]
-
-            # All other cases, grab the face-up card we are clicking on
-            self.held_cards = [primary_card]
-            # Save the position
-            self.held_cards_original_position = [self.held_cards[0].position]
-            # Put on top in drawing order
-            self.pull_to_top(self.held_cards[0])
-
-    def on_mouse_release(self, x: float, y: float, button: int,
-                         modifiers: int):
-        """ Called when the user presses a mouse button. """
-        # If we don't have any cards, who cares
-        if len(self.held_cards) == 0:
-            return
-
-        # We are no longer holding cards
-        self.held_cards = []
-
-    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
-        """ User moves mouse """
-        pass
 
 class Card(arcade.Sprite):
     """ Card sprite """
