2a1c6468663832f0416b04f28977174199e3fdb6
cjrinaldi <cjrinald@uvm.edu>
2023-11-02 19:16:46 -0400

    matching()

--- Player.py
+++ Player.py
@@ -5,6 +5,7 @@ from collections import Counter
 
 
 class HandStrength(Enum):
+    DEFAULT = 0
     HIGH_CARD = 1
     ONE_PAIR = 2
     TWO_PAIR = 3
@@ -16,37 +17,39 @@ class HandStrength(Enum):
     STRAIGHT_FLUSH = 9
     ROYAL_FLUSH = 10
 
+
 class Player:
     def __init__(self):
         self.name = None
         self.hand = []
         self.stack = 0
-        self.is_computer_player = True #defaults to computer player
-    
+        self.is_computer_player = True  # defaults to computer player
+        self.showdown = []
+        self.handStrength = HandStrength.DEFAULT
+
     def get_name(self):
         return self.name
-    
+
     def get_hand(self):
         return self.hand
-    
+
     def get_stack(self):
         return self.stack
-    
+
     def get_player_type(self):
         return self.is_computer_player
-    
+
     def set_stack(self, stack):
         self.stack = stack
-    
+
     def set_name(self, name):
         self.name = name
-    
+
     def set_hand(self, hand):
         self.hand = hand
-    
+
     def set_computer_player(self, boolean_value):
         self.is_computer_player = boolean_value
-
 
     def turn(self, community_cards):
         # return decision (bet, check, fold)
@@ -59,7 +62,6 @@ class Player:
         hand_strength = self.calculate_strength(combined_cards)
         return hand_strength
 
-
     def evaluate_hand(self, community_cards):
         #  takes in community_cards, combines them with self.hand, and returns the ENUM for the player's hand.
 
@@ -70,11 +72,11 @@ class Player:
     def make_decision(self, community_cards):
         # could use evaluate_strength and evaluate_hand as part of decision
         # random decision for now
-        #EDIT needs to return a bet value if decision is bet. This can just be zero if the decision is not bet
+        # EDIT needs to return a bet value if decision is bet. This can just be zero if the decision is not bet
         decisions = ["bet", "check", "fold"]
         choice = random.choice(decisions)
         if choice == "bet":
-            bet_value = 5 * random.randint(1, 10) #THIS IS A PLACEHOLDER
+            bet_value = 5 * random.randint(1, 10)  # THIS IS A PLACEHOLDER
         else:
             bet_value = 0
 
@@ -106,43 +108,41 @@ class Player:
             return HandStrength.ONE_PAIR
         else:
             return HandStrength.HIGH_CARD
-        
+
     def get_hand_type(self, cards):
         # Implement hand type evaluation logic
         # Return the appropriate ENUM from HandStrength
+        pass
 
-        def is_royal_flush(self, cards):
-            # Check if it's a Royal Flush (A, K, Q, J, 10 of the same suit)
-            pass
-
-        def is_straight_flush(self, cards):
-            # Check if it's a Straight Flush
-            pass
-
-        def is_four_of_a_kind(self, counts):
-            # Check if it's Four of a Kind
-            pass
-
-        def is_full_house(self, counts):
-            # Check if it's a Full House
-            pass
-
-        def is_flush(self, cards):
-            # Check if it's a Flush
-            pass
-
-        def is_straight(self, cards):
-            # Check if it's a Straight
-            pass
-
-        def is_three_of_a_kind(self, counts):
-            # Check if it's Three of a Kind
-            pass
-
-        def is_two_pair(self, counts):
-            # Check if it's Two Pair
-            pass
-
-        def is_one_pair(self, counts):
-            # Check if it's One Pair
-            return 2 in counts.values()
+    def matching(self, cards):
+        # memory for matching cards, stores a list of a list of cards
+        matching = [[], [], [], [], [], [], [], [], [], [], [], [], []]
+        # memory for number of pairs
+        pairs = 0
+        pair_values = []
+        # iterate through given cards and sort them by adding them to matching[[],[],...[] by index of value
+        for i in cards:
+            matching[cards[i].get_value()].append(cards[i])
+        # iterate through matching
+        for e, i in enumerate(matching):
+            # if the list of cards sorted by index contains
+            if len(i) == 4:
+                self.handStrength = HandStrength.FOUR_OF_A_KIND
+                self.showdown = i
+                return HandStrength.FOUR_OF_A_KIND
+            if len(i) == 3:
+                self.handStrength = HandStrength.THREE_OF_A_KIND
+                self.showdown = i
+                return HandStrength.THREE_OF_A_KIND
+            if len(i) == 2:
+                pairs = +1
+                pair_values.append(e)
+        if pairs == 1:
+            self.handStrength = HandStrength.ONE_PAIR
+            self.showdown = matching[pair_values.pop()]
+            return HandStrength.ONE_PAIR
+        if pairs == 2:
+            self.handStrength = HandStrength.TWO_PAIR
+            self.showdown = matching[pair_values.pop()]
+            self.showdown.append(matching[pair_values.pop()])
+            return HandStrength.TWO_PAIR
