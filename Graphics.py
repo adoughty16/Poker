8e732e00a7dc18fca83d8c3ebb7102a0a28f5b88
xantia.kansil <xantia.kansil@uvm.edu>
2023-11-09 17:45:45 -0500

    cleaning

--- Graphics.py
+++ Graphics.py
@@ -255,9 +255,6 @@ class WelcomeView(arcade.View):
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         """ If the user presses the mouse button, start the game. """
-        #game_view = GameView(self.selected_players, self.selected_host)
-        #game_view.setup()
-        #self.window.show_view(game_view)
         pass
         # pass in self.selected_host() and self.selected_players to the next window rather than using game-state
         # so that GameView can access those values and also create its own game-state object
